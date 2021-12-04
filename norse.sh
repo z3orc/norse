1c8dd60d49bb9555841d372d82db2559db59598f
z3orc <pahasseleng@gmail.com>
2021-12-04 13:41:32 +0100

    Added console function

--- norse.sh
+++ norse.sh
@@ -357,6 +357,11 @@ function upgrade {
 
 }
 
+function console {
+    source norse.config
+    tmux a -t valheim-$ID
+}
+
 case "$1" in
         setup)
                 setup
@@ -376,6 +381,9 @@ case "$1" in
         upgrade)
                 upgrade "${2}"
                 ;;
+        console)
+                console
+                ;;
         *)
-                echo "Usage: $0 {start|resume|stop|upgrade|backup|setup}"
+                echo "Usage: ./norse.sh {start|stop|upgrade|backup|console|setup}"
 esac
