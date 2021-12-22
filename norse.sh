ce7e84afc1d6bfb837bbbb182af37c1204314a00
z3orc <z3orc@users.noreply.github.com>
2021-12-22 18:50:45 +0100

    Server should now shutdown correctly

--- norse.sh
+++ norse.sh
@@ -97,26 +97,37 @@ function bootServerLoop {
 
     cd $DIR/serverfiles/valheim_server || exit
 
-    i=0
-    while [ $i -lt 4 ] 
-    do
-            echo "Server started"
-            rm -rf $DIR/.offline
-                export templdpath=$LD_LIBRARY_PATH
-                export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
-                export SteamAppId=892970
+    # i=0
+    # while [ $i -lt 4 ] 
+    # do
+    #         echo "Server started"
+    #         rm -rf $DIR/.offline
+    #             export templdpath=$LD_LIBRARY_PATH
+    #             export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+    #             export SteamAppId=892970
 
-                # Tip: Make a local copy of this script to avoid it being overwritten by steam.
-                # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
-                # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewal>
-                ./valheim_server.x86_64 -name "$NAME" -port 2456 -world "$WORLD" -password "$PASS" -public "$PUBLIC" -savedir "$DIR/serverfiles"
+    #             # Tip: Make a local copy of this script to avoid it being overwritten by steam.
+    #             # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+    #             # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewal>
+    #             ./valheim_server.x86_64 -name "$NAME" -port 2456 -world "$WORLD" -password "$PASS" -public "$PUBLIC" -savedir "$DIR/serverfiles"
 
-                export LD_LIBRARY_PATH=$templdpath
-            touch $DIR/.offline
-            echo "Server stopped"
-            sleep 120;
-            i=$[$i+1]
-    done
+    #             export LD_LIBRARY_PATH=$templdpath
+    #         touch $DIR/.offline
+    #         echo "Server stopped"
+    #         sleep 120;
+    #         i=$[$i+1]
+    # done
+
+    export templdpath=$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+    export SteamAppId=892970
+
+    # Tip: Make a local copy of this script to avoid it being overwritten by steam.
+    # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+    # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewal>
+    ./valheim_server.x86_64 -name "$NAME" -port 2456 -world "$WORLD" -password "$PASS" -public "$PUBLIC" -savedir "$DIR/serverfiles"
+
+    export LD_LIBRARY_PATH=$templdpath
 }
 
 # Functions for controlling the server
@@ -301,7 +312,7 @@ function stop {
         fi
     done
 
-    if isPortBinded 2456 || isServerRunning || isSessionRunning valheim-$ID; then
+    if isSessionRunning valheim-$ID; then
         logBad "Could not halt server."
         setServerState online
     fi
